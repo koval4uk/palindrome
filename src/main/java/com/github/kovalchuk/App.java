76bc1ff1a1b2b540100e04fb17f4f3169bac5a4c
Artur Kovalchuk <akovalchuk@egp.com.ua>
2019-06-01 12:43:31 +0300

    Add task logic

--- src/main/java/com/github/kovalchuk/App.java
+++ src/main/java/com/github/kovalchuk/App.java
@@ -2,49 +2,101 @@ package com.github.kovalchuk;
 
 import org.apache.log4j.Logger;
 
-import java.util.StringJoiner;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.BreakIterator;
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 public class App {
     final static Logger logger = Logger.getLogger(App.class);
+    private static final boolean TEST_MODE = true;
 
     public static void main(String[] args) {
-        RequestDTO requestDTO = new RequestDTO();
+        if (TEST_MODE) {
+            logger.error("Start in TEST_MODE");
+            args = new String[]{"input.txt", "output.txt"};
+        }
+
         if (args.length == 2) {
-            requestDTO.setInputFile(args[0]);
-            requestDTO.setOutputFile(args[1]);
+            Path inputPath = Paths.get(args[0]);
+
+            File inputFile = inputPath.toFile();
+
+            if (inputFile.exists()) {
+                logger.debug("Start processing file \n");
+
+                Set<String> words = new HashSet<>();
+                try {
+                    Files.lines(inputFile.toPath()).forEach(line -> {
+                        words.addAll(getWords(line));
+                        logger.debug(line);
+                    });
+
+                    logger.debug("Finish processing file, find '" + words.size() + "'words\n");
+
+                    // find palindrome
+                    SortedSet<String> palindromes = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+                    for (String word : words) {
+                        if (isPalindromString(word) && word.length() >= 2) {
+                            palindromes.add(word);
+                        }
+
+                    }
+                    logger.debug("Find '" + palindromes.size() + "'palindromes\n");
+
+                    logger.debug("Start writting file");
+
+                    Collections.sort(new ArrayList<>(palindromes), Comparator.reverseOrder());
+
+                    Path outputPath = Paths.get(args[1]);
+                    Files.write(outputPath, String.join("\n", palindromes).getBytes());
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                logger.error("!!!!!!!!!");
+
+
+            }
+
         } else {
             logger.error("Invalid input params. Need input: [java -jar Palindrom.jar \"input.txt\" \"result.txt\"]");
         }
 
-        logger.info("Hello World! " + requestDTO.toString());
+        logger.info("End task");
     }
 
-    static class RequestDTO {
-        String inputFile;
-        String outputFile;
-
-        public String getInputFile() {
-            return inputFile;
-        }
-
-        public void setInputFile(String inputFile) {
-            this.inputFile = inputFile;
-        }
-
-        public String getOutputFile() {
-            return outputFile;
-        }
-
-        public void setOutputFile(String outputFile) {
-            this.outputFile = outputFile;
-        }
-
-        @Override
-        public String toString() {
-            return new StringJoiner(", ", RequestDTO.class.getSimpleName() + "[", "]")
-                .add("inputFile='" + inputFile + "'")
-                .add("outputFile='" + outputFile + "'")
-                .toString();
-        }
+    public static boolean isPalindromString(String text) {
+        String reverse = reverse(text);
+        return text.equals(reverse);
     }
+
+    public static String reverse(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+        return input.charAt(input.length() - 1) + reverse(input.substring(0, input.length() - 1));
+    }
+
+    public static Set<String> getWords(String text) {
+        Set<String> words = new HashSet<>();
+        BreakIterator breakIterator = BreakIterator.getWordInstance();
+        breakIterator.setText(text);
+        int lastIndex = breakIterator.first();
+        while (BreakIterator.DONE != lastIndex) {
+            int firstIndex = lastIndex;
+            lastIndex = breakIterator.next();
+            if (lastIndex != BreakIterator.DONE && Character.isLetterOrDigit(text.charAt(firstIndex))) {
+                words.add(text.substring(firstIndex, lastIndex));
+            }
+        }
+        return words;
+    }
+
 }
